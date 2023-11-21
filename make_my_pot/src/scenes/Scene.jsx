614418eef1ae027d627fe313435f526882567efb
bhatiarush <arush.bhatia@smallcase.com>
2023-11-21 20:54:23 +0530

    updating structure 2

--- make_my_pot/src/scenes/Scene.jsx
+++ make_my_pot/src/scenes/Scene.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Scene = () => {
   return (
-    <div className="m" style={{ height: "400px", flex: 4, marginTop: "80px" }}>
+    <div className="m" style={{ flex: 4, marginTop: "80px" }}>
       Scene
     </div>
   );
