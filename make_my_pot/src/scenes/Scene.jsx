bbce046f4a9486e164765501f9112bc1742006b3
bhatiarush <arush.bhatia@smallcase.com>
2023-11-25 17:50:44 +0530

    refactor

--- make_my_pot/src/scenes/Scene.jsx
+++ make_my_pot/src/scenes/Scene.jsx
@@ -2,10 +2,7 @@ import React from "react";
 
 const Scene = ({ children }) => {
   return (
-    <div
-      className="m"
-      style={{ height: "400px", height: "100%", padding: "0 20px" }}
-    >
+    <div style={{ height: "400px", height: "100%", padding: "0 20px" }}>
       {children}
     </div>
   );
