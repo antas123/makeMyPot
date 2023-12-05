82bf3b7b326e88138e88fc291da719beb2f17adf
bhatiarush <arush.bhatia@smallcase.com>
2023-12-05 20:44:16 +0530

    table and piecard

--- make_my_pot/src/components/extras/TableCard/TableCard.jsx
+++ make_my_pot/src/components/extras/TableCard/TableCard.jsx
@@ -2,7 +2,7 @@ import { Typography } from "@mui/material";
 import React from "react";
 import styles from "./TableCard.module.css";
 
-const TableCard = ({ title, data, color }) => {
+const TableCard = ({ title, data, color, type }) => {
   return (
     <div className={styles.statCardContainer}>
       <Typography
@@ -11,14 +11,27 @@ const TableCard = ({ title, data, color }) => {
         {title}
       </Typography>
       <div className={styles.statCardInfoContainer}>
-        {data?.map((x) => {
-          return (
-            <div className={styles.tableDiv}>
-              <div>{x.title}</div>
-              <div style={{ width: "100px" }}>Rs {x.value}</div>
-            </div>
-          );
-        })}
+        {data.length ? (
+          data?.map((x) => {
+            return (
+              <div className={styles.tableDiv}>
+                <div>{x.title}</div>
+                <div style={{ width: "100px" }}>Rs {x.value}</div>
+              </div>
+            );
+          })
+        ) : (
+          <div
+            style={{
+              height: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            Please enter {type} to see here
+          </div>
+        )}
       </div>
     </div>
   );
